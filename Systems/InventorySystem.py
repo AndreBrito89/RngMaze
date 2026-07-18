073183b6c6e87ea03ada678ce4004b2cd0349fb8
André <andre.brito.arnaud@gmail.com>
2026-07-18 17:58:27 -0300

    pequenas correcoes em obtain_weapon e obtain_armor, adicionados obtain_potion e use_potion

--- Systems/InventorySystem.py
+++ Systems/InventorySystem.py
@@ -1,9 +1,10 @@
-import UISystem
+from Systems import UISystem
 
 # player gets a weapon drop
 def obtain_weapon(player, newWeapon):
     # checks if the player have an available slot
-    if player.inventoryWeapon == None:
+    if player.has_weapon_slot():
+        print(f"Voce recebeu {newWeapon.weaponName}!")
         player.inventoryWeapon = newWeapon
         return
     playerDiscardedWeapon = UISystem.choose_weapon_to_discard(player, newWeapon)
@@ -11,7 +12,7 @@ def obtain_weapon(player, newWeapon):
     match playerDiscardedWeapon:
         case "1":
             print(f"Voce descartou {player.equippedWeapon.weaponName}!\n {player.inventoryWeapon.weaponName} foi equipada!")
-            player.equippedWeapon = player.inventoryWeapon
+            player.swap_weapons()
             player.inventoryWeapon = newWeapon
         case "2":
             print(f"Voce descartou {player.inventoryWeapon.weaponName}!")
@@ -20,9 +21,11 @@ def obtain_weapon(player, newWeapon):
             print(f"Voce descartou {newWeapon.weaponName}!")
             pass
 
+# player gets an armor drop
 def obtain_armor(player, newArmor):
     # checks if the player have an available slot
-    if player.inventoryArmor == None:
+    if player.has_armor_slot():
+        print(f"Voce recebeu {newArmor.armorName}")
         player.inventoryArmor = newArmor
         return
     playerDiscardedArmor = UISystem.choose_armor_to_discard(player, newArmor)
@@ -30,7 +33,7 @@ def obtain_armor(player, newArmor):
     match playerDiscardedArmor:
         case "1":
             print(f"Voce descartou {player.equippedArmor.armorName}!\n {player.inventoryArmor.armorName} foi equipada!")
-            player.equippedArmor = player.inventoryArmor
+            player.swap_armors()
             player.inventoryArmor = newArmor
         case "2":
             print(f"Voce descartou {player.inventoryArmor.armorName}!")
@@ -40,3 +43,48 @@ def obtain_armor(player, newArmor):
             pass
 
     
+# player gets a potion drop
+def obtain_potion(player, newPotion):
+
+    # adds potion if player does have an available slot
+    if player.has_potion_slot():
+        print(f"Voce recebeu {newPotion.potionName}")
+        player.potions.append(newPotion)
+        return
+    
+    # selects a potion
+    selectedPotion = UISystem.choose_new_potion_option(player, newPotion)
+
+    # asks if player wants to consume or discard the selected potion
+    potionAction = UISystem.choose_potion_action()
+
+    if potionAction == "1" and selectedPotion < 6:
+        print(f"Voce recebeu +{player.potions[selectedPotion - 1].potionRegenPoints} {player.potions[selectedPotion - 1].potionType}")
+        player.use_potion(player.potions[selectedPotion - 1])
+        player.potions[selectedPotion - 1] = newPotion
+    
+    elif potionAction == "1" and selectedPotion == 6:
+        print(f"Voce recebeu +{newPotion.potionRegenPoints} {newPotion.potionType}")
+        player.use_potion(newPotion)
+    
+    elif potionAction == "2" and selectedPotion < 6:
+        print(f"Voce descartou {player.potions[selectedPotion - 1].potionName} e recebeu {newPotion.potionName}.")
+        player.potions[selectedPotion - 1] = newPotion
+    elif potionAction == "2" and selectedPotion == 6:
+        print(f"Voce descartou {newPotion.potionName}")
+
+def use_potion(player):
+    # checks if the player has any potions
+    if len(player.potions) == 0:
+        print("Voce nao possui pocoes!")
+        return
+
+    UISystem.show_player_potions(player)
+
+    selectedPotion = UISystem.select_inventory_potion(player)
+    # assigns the removed potion to the potion variable
+    potion = player.potions.pop(selectedPotion - 1)
+
+    player.use_potion(potion)
+
+    print(f"Voce consumiu {potion.potionName}!")
